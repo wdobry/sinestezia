fc2d0d08677bf111e523f5b8a1f3e2f7a282c293
Wojciech Dobry <wdobry@gmail.com>
2023-02-13 11:00:04 +0000

    Update 8 files

--- src/Api.js
+++ src/Api.js
@@ -12,7 +12,7 @@ export const ApiTest = () => {
 
   const [inputValue, setInputValue] = useState({
     soundHandle: "raeisla",
-    releaseSlug: "thank-you-im-sorry-i-love-you"
+    releaseSlug: "thank-you-im-sorry-i-love-you",
   });
   const inputRef = useRef();
 
@@ -31,11 +31,11 @@ export const ApiTest = () => {
       document: getRelease,
       variables: {
         soundHandle: inputValue.soundHandle,
-        releaseSlug: inputValue.releaseSlug
+        releaseSlug: inputValue.releaseSlug,
       },
       requestHeaders: {
-        "X-Sound-Client-Key": "91f01600-9b59-4dbb-a55d-e391a9f5f7c4"
-      }
+        "X-Sound-Client-Key": "91f01600-9b59-4dbb-a55d-e391a9f5f7c4",
+      },
     })
       .then((data) => {
         setIsLoaded(true);
@@ -75,23 +75,11 @@ export const ApiTest = () => {
 const Scene = ({ filename = "canvas", releaseData }) => {
   const gl = useThree((state) => state.gl);
 
-  const handleSave = () => {
-    const link = document.createElement("a");
-    link.setAttribute("download", `${filename}.png`);
-    link.setAttribute(
-      "href",
-      gl.domElement
-        .toDataURL("image/png")
-        .replace("image/png", "image/octet-stream")
-    );
-    link.click();
-  };
-
   const texture = useTexture(releaseData.coverImage.url);
 
   const coverRef = useRef();
   useFrame((state, delta) => {
-    // coverRef.current.rotation.y += delta * 0.5;
+    coverRef.current.rotation.y += delta * 0.5;
   });
 
   const [play, setPlay] = useState(false);
@@ -99,12 +87,12 @@ const Scene = ({ filename = "canvas", releaseData }) => {
   return (
     <>
       <PerspectiveCamera position={[0, 0, 1]} makeDefault />
-      <color attach="background" args={["gray"]} />
+      <color attach="background" args={["black"]} />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Text color="white" anchorX="center" anchorY="middle" fontSize="0.1">
+      {/* <Text color="white" anchorX="center" anchorY="middle" fontSize="0.1">
         {releaseData.title}, {releaseData.artist.name}
-      </Text>
+      </Text> */}
       <PlaySound url={releaseData.track.revealedAudio.url} play={play} />
       <group
         ref={coverRef}
@@ -113,7 +101,7 @@ const Scene = ({ filename = "canvas", releaseData }) => {
       >
         <Suspense fallback={null}>
           <mesh>
-            <boxGeometry args={[1, 1, 0.02]} />
+            <boxGeometry args={[0.8, 0.8, 0.02]} />
             <meshStandardMaterial map={texture} />
           </mesh>
         </Suspense>
